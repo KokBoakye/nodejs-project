064390be7c4bce0f3fb3db62891d5124724cd951
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-10-15 21:45:23 +0100

    attached SSM parameters

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,11 @@
-# output "instance_ip" {
-#   description = "The IP address of the instance"
-#   value       = aws_instance.application_server.public_ip
+output "instance_ip" {
+  description = "The IP address of the instance"
+  value       = aws_instance.application_server.public_ip
 
-# }
+}
+
+output "instance_ids" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.application_server.id
+
+}
